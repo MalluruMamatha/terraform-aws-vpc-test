fd9df0a00fa6afab6d1988fff4a0a25fde30f619
MalluruMamatha <mamatha.mm939@gmail.com>
2024-07-06 20:50:36 +0530

    vpc-test

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+output "zones_info" {
+    value = module.vpc_test.azs
+  
+}
+
+output "vpc_id" {
+  value = module.vpc_test.vpc_id
+}
+
+output "public_subnet_list" {
+  value = module.vpc_test.public_subnet_ids
+}
+
+output "igw_id" {
+  value = module.vpc_test.igw_id
+}
---
+++ variable.tf
@@ -0,0 +1,39 @@
+variable "project_name" {
+    type = string
+  default = "expense"
+}
+variable "environment" {
+    type = string
+  default = "dev"
+}
+
+variable "common_tags" {
+  type = map
+  default = {
+    project = "expense"
+    terraform = "true"
+    environment = "dev"
+  }
+}
+
+variable "public_subnet_cidrs" {
+  type = list
+  default = ["10.0.16.0/24","10.0.17.0/24"]
+  
+}
+
+variable "private_subnet_cidrs" {
+   type = list
+  default = ["10.0.24.0/24","10.0.25.0/24"]
+  
+}
+
+variable "database_subnet_cidrs" {
+   type = list
+  default = ["10.0.32.0/24","10.0.33.0/24"]
+  
+}
+
+variable "is_peering_required" {
+  default = true
+}
