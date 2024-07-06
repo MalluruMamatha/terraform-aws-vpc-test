70540b08b0f86f2e9619c316978acbff62bd0f2a
MalluruMamatha <mamatha.mm939@gmail.com>
2024-07-06 20:54:51 +0530

    vpc test

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "vpc_test" {
-source = "../terraform-aws-vpc"
+#source = "../terraform-aws-vpc"
+source = "https://github.com/MalluruMamatha/terraform-aws-vpc.git?ref=main"
 project_name = var.project_name
 environment = var.environment
 common_tags = var.common_tags
